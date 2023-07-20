269ea61632de153644366f53489423d27ef082dc
Leone67 <56906270+Leone67@users.noreply.github.com>
2023-07-20 13:14:38 +1000

    Updated cluster spec

--- Test_SB.py
+++ Test_SB.py
@@ -1,7 +1,54 @@
 # Databricks notebook source
+file_path = os.path.abspath(os.path.dirname("__file__"))
+print(file_path)
+
+# COMMAND ----------
+
 # MAGIC %fs
 # MAGIC
-# MAGIC ls s3a://datalake-shared-resources-etl-artifact-prd-twycaj6ki1az/
+# MAGIC cp s3://datalake-shared-resources-etl-artifact-prd-twycaj6ki1az/app/bronze/demo_app/017390273/pipeline-package.zip dbfs:/Users/leon.eller@databricks.com/zips/pipeline-package.zip
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC
+# MAGIC cp dbfs:/Users/leon.eller@databricks.com/zips/pipeline-package.zip s3://datalake-shared-resources-etl-artifact-prd-twycaj6ki1az/app/bronze/demo_app/017390273/pipeline-package.zip
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC
+# MAGIC cp /dbfs/Users/leon.eller@databricks.com/zips/pipeline-package.zip /Workspace/Shared/pipeline-package.zip
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC
+# MAGIC curl https://github.com/LeoneGarage/sb-disco/raw/main/db_submit.zip -L -o /dbfs/Users/leon.eller@databricks.com/zips/db_submit.zip
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC
+# MAGIC curl https://github.com/LeoneGarage/sb-disco/raw/main/lakehouselib-latest.zip -L -o /dbfs/Users/leon.eller@databricks.com/zips/lakehouselib-latest.zip
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC
+# MAGIC cp s3://datalake-shared-resources-etl-artifact-prd-twycaj6ki1az/app/bronze/demo_app/017390273/libs/lakehouselib-latest.zip s3://datalake-shared-resources-etl-artifact-prd-twycaj6ki1az/packages/sportsbet/lakehouselib/pip/lakehouselib-latest.zip
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC
+# MAGIC ls s3://datalake-shared-resources-etl-artifact-prd-twycaj6ki1az/packages/sportsbet/lakehouselib/pip/
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC
+# MAGIC ls s3a://datalake-python-repo-ap-southeast-2-dv2t4qi6u0ds/packages/sportsbet/lakehouselib/pip/lakehouselib-latest.zip
 
 # COMMAND ----------
 
@@ -58,9 +105,8 @@ os.environ["DATABRICKS_TOKEN"] = sc.getLocalProperty("spark.databricks.token")
 # MAGIC             "environment": "prod",
 # MAGIC             "cicd_notebook_path_prefix": "CICD/30_delta_prod/"
 # MAGIC         },
-# MAGIC         "node_type_id":"i3.xlarge",
-# MAGIC         "driver_node_type_id":"m5.xlarge",
-# MAGIC         "enable_elastic_disk":false,
+# MAGIC         "instance_pool_id":"0710-115244-eases83-pool-85r6lc2b",
+# MAGIC         "driver_instance_pool_id":"0710-115450-strip84-pool-hqm6madv",
 # MAGIC         "data_security_mode":"NONE",
 # MAGIC         "runtime_engine":"STANDARD",
 # MAGIC         "autoscale": {
@@ -72,7 +118,7 @@ os.environ["DATABRICKS_TOKEN"] = sc.getLocalProperty("spark.databricks.token")
 # MAGIC # export DATABRICKS_HOST=https://e2-demo-field-eng.cloud.databricks.com
 # MAGIC # export DATABRICKS_TOKEN=<PAT Token>
 # MAGIC mkdir -p ~/app/bronze/bronze_bwm.dummy_two/db_submit; cp /dbfs/Users/leon.eller@databricks.com/zips/db_submit.zip ~/app/bronze/bronze_bwm.dummy_two/db_submit/db_submit.zip;unzip -o ~/app/bronze/bronze_bwm.dummy_two/db_submit/db_submit.zip -d ~/app/bronze/bronze_bwm.dummy_two/db_submit
-# MAGIC ~/app/bronze/bronze_bwm.dummy_two/db_submit/bin/db-submit --wait-for-completion false --source-zip s3://datalake-shared-resources-etl-artifact-prd-twycaj6ki1az/app/bronze/demo_app/017390273/pipeline-package.zip --dest-zip /tmp/app/bronze/bronze_bwm.dummy_two --cluster-spec "$CLUSTER_SPEC" --conf spark.app.name=bronze_bwm.dummy_two-2023-07-07T04:38:10.717 --conf spark.yarn.stagingDir=hdfs://$(hostname -f):8020/user/hadoop --conf spark.yarn.appMasterEnv.SPARK_HOME=/usr/lib/spark --conf spark.yarn.submit.waitAppCompletion=true --conf spark.port.maxRetries=1000 --conf spark.yarn.tags=bronze_bwm.dummy_two-2023-07-07T04:38:10.717 --conf spark.executor.asyncEagerFileSystemInit.paths=s3://datalake-python-repo-ap-southeast-2-dv2t4qi6u0ds --conf spark.kryoserializer.buffer.max=1024m --conf spark.default.parallelism=512 --conf spark.sql.adaptive.coalescePartitions.initialPartitionNum=512 --conf spark.dynamicAllocation.maxExecutors=6 --py-files /tmp/app/bronze/bronze_bwm.dummy_two/pipeline-package.zip,s3a://datalake-python-repo-ap-southeast-2-dv2t4qi6u0ds/packages/sportsbet/lakehouselib/pip/lakehouselib-latest.zip --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.2,org.apache.spark:spark-avro_2.12:3.3.2,com.fasterxml.jackson.core:jackson-databind:2.12.0 /tmp/app/bronze/bronze_bwm.dummy_two/main.py  --job demo_app --job-type bronze --job-args region_name=ap-southeast-2 pipeline_name=bronze_bwm.dummy_two pipeline_run_id=bronze_bwm.dummy_two-2023-07-07T04:38:10.717 kafka_topic=dummy_two load_type=INCR dependencies=bronze_bwm.dummy_one bronze_bwm.dummy_one.batchId=1688383197 bronze_bwm.dummy_one.delta_table_path=s3://dl-bronze-bwm-pii-prd-lsmjik7uyv8/bronze_bwm/dummy_one bronze_bwm.dummy_one.None=None
+# MAGIC ~/app/bronze/bronze_bwm.dummy_two/db_submit/bin/db-submit --wait-for-completion false --source-zip s3://datalake-shared-resources-etl-artifact-prd-twycaj6ki1az/app/bronze/demo_app/017390273/pipeline-package.zip --dest-zip /tmp/app/bronze/bronze_bwm.dummy_two --cluster-spec "$CLUSTER_SPEC" --conf spark.app.name=bronze_bwm.dummy_two-2023-07-07T04:38:10.717 --conf spark.yarn.stagingDir=hdfs://$(hostname -f):8020/user/hadoop --conf spark.yarn.appMasterEnv.SPARK_HOME=/usr/lib/spark --conf spark.yarn.submit.waitAppCompletion=true --conf spark.port.maxRetries=1000 --conf spark.yarn.tags=bronze_bwm.dummy_two-2023-07-07T04:38:10.717 --conf spark.executor.asyncEagerFileSystemInit.paths=s3://datalake-python-repo-ap-southeast-2-dv2t4qi6u0ds --conf spark.kryoserializer.buffer.max=1024m --conf spark.default.parallelism=512 --conf spark.sql.adaptive.coalescePartitions.initialPartitionNum=512 --conf spark.dynamicAllocation.maxExecutors=6 --instance-profile-arn arn:aws:iam::591792984463:instance-profile/e2-data-platforms-e2instanceprofileiam-DPPII2ProducerInstanceProfile-VsL8TCbEaVjv --py-files s3a://datalake-shared-resources-etl-artifact-prd-twycaj6ki1az/app/bronze/demo_app/017390273/pipeline-package.zip,s3a://datalake-shared-resources-etl-artifact-prd-twycaj6ki1az/packages/sportsbet/lakehouselib/pip/lakehouselib-latest.zip --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.2,org.apache.spark:spark-avro_2.12:3.3.2,com.fasterxml.jackson.core:jackson-databind:2.12.0,awswrangler /tmp/app/bronze/bronze_bwm.dummy_two/main.py  --job demo_app --job-type bronze --job-args region_name=ap-southeast-2 pipeline_name=bronze_bwm.dummy_two pipeline_run_id=bronze_bwm.dummy_two-2023-07-07T04:38:10.717 kafka_topic=dummy_two load_type=INCR dependencies=bronze_bwm.dummy_one bronze_bwm.dummy_one.batchId=1688383197 bronze_bwm.dummy_one.delta_table_path=s3://dl-bronze-bwm-pii-prd-lsmjik7uyv8/bronze_bwm/dummy_one bronze_bwm.dummy_one.None=None
 
 # COMMAND ----------
 
